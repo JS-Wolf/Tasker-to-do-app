acb294e041d8afe7b0a0952a0dc6b33c9749153c
JS-Wolf <47048385+JS-Wolf@users.noreply.github.com>
2022-02-09 12:52:32 -0500

    Integration of React

--- todo_app/todo_app/urls.py
+++ todo_app/todo_app/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('',TemplateView.as_view(template_name='index.html'))
 ]
